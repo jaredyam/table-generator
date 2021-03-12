d44b679619ecc7096a57f22f277c19b6245db83d
jaredyam <jihonren@gmail.com>
2021-03-12 22:43:52 +0800

    Update execution condition

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ ! -d csv-converter ]]; then
+if [ -d csv_converter ]; then
+    echo "fatal: dependent package 'csv-converter' already exists"
+else
     git clone https://github.com/jaredyam/csv-converter.git &&
         mv csv-converter/csv_converter ./ &&
         rm -rf csv-converter
